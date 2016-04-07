a5e60ee61f8819a6c61235a857f7dc9e4ca97181
Dan Andrei <andreidani96@gmail.com>
2016-04-07 17:51:38 +0300

    Added events support and pushState.

--- index.js
+++ index.js
@@ -6,9 +6,52 @@
  *
  */
 exports.init = function (config, ready) {
-	var self = this;
+    var self = this;
 
-	ready();
+    // init streams
+    self._streams = self._streams || {};
+
+    if (config.events) {
+
+        Object.keys(config.events).forEach(function (eventName) {
+            config.events[eventName].forEach(function (elm) {
+                if (global.addEventListener) {
+                    global.addEventListener(elm.on, function (event) {
+                        var data = {};
+
+                        // handle popstate events
+                        if (event.type === 'popstate') {
+                            data.url = global.location.pathname;
+                        }
+
+                        // create event stream
+                        var eventStream = self._streams[eventName];
+                        if (!eventStream) {
+                            eventStream = self.flow(eventName);
+
+                            var handler = function (err) {
+                                if (err) {
+                                    console.error(err);
+                                }
+                                delete self._streams[eventName];
+                            };
+
+                            // cache stream
+                            self._streams[eventName] = eventStream;
+
+                            eventStream.on('error', handler);
+                            eventStream.on('end', handler);
+                            eventStream.on('data', function nirvana () {});
+                        }
+
+                        eventStream.write(data);
+                    });
+                }
+            });
+        });
+    }
+
+    ready();
 };
 
 /**
@@ -25,6 +68,16 @@ exports.init = function (config, ready) {
  * @param {Function} next The next function.
  */
 exports.pushState = function (options, data, next) {
+    var url = data.url || options.url || options._.url;
 
-	return next(null, data);
+    if (!url) {
+        return next(null, data);
+    }
+
+    // update history if url is different
+    if (global.location && url !== global.location.pathname) {
+        global.history.pushState(0, 0, url);
+    }
+
+    return next(null, data);
 };
